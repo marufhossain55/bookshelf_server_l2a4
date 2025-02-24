5ac5bd1e985393a0ad4e157f2e2539c28136771b
Your Name <you@example.com>
2025-02-24 12:17:04 +0600

    utils added

---
+++ src/app/utils/sendResponse.ts
@@ -0,0 +1,19 @@
+import { Response } from 'express';
+type TSuccessResponse<T> = {
+  success: boolean;
+  message: string;
+  statusCode: number;
+  data: T | T[] | null;
+  meta: T | null;
+};
+
+const sendResponse = <T>(res: Response, data: TSuccessResponse<T>) => {
+  res.status(data.statusCode).json({
+    success: true,
+    message: data.message,
+    statusCode: data.statusCode,
+    data: data.data,
+    meta: data.meta,
+  });
+};
+export default sendResponse;
