3544c71a34fc4079b2a3ae31c25431fd4257aa8d
Your Name <you@example.com>
2025-02-24 20:28:21 +0600

    payment structure added & surjopay initialized

--- src/app/config/index.ts
+++ src/app/config/index.ts
@@ -12,4 +12,12 @@ export default {
   jwt_refresh_expires_in: process.env.JWT_REFRESH_EXPIRES_IN,
   bcrypt_salt_rounds: process.env.BCRYPT_SALT_ROUNDS,
   NODE_ENV: process.env.NODE_ENV,
+
+  sp: {
+    sp_endpoint: process.env.SP_ENDPOINT,
+    sp_username: process.env.SP_USERNAME,
+    sp_password: process.env.SP_PASSWORD,
+    sp_prefix: process.env.SP_PREFIX,
+    sp_return_url: process.env.SP_RETURN_URL,
+  },
 };
